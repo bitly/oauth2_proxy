fd1e7e2715e79bf2b98736771cfa204aa0561829
Manoj Kumar <manoj.kumar@florahospitality.com>
2016-02-13 17:14:42 +0530

    Updated hotelx.go

--- providers/hotelx.go
+++ providers/hotelx.go
@@ -9,37 +9,37 @@ import (
 	"github.com/bitly/oauth2_proxy/api"
 )
 
-type LinkedInProvider struct {
+type HotelxProvider struct {
 	*ProviderData
 }
 
-func NewLinkedInProvider(p *ProviderData) *LinkedInProvider {
-	p.ProviderName = "LinkedIn"
+func NewHotelxProvider(p *ProviderData) *HotelxProvider {
+	p.ProviderName = "Hotelx"
 	if p.LoginURL.String() == "" {
 		p.LoginURL = &url.URL{Scheme: "https",
-			Host: "www.linkedin.com",
-			Path: "/uas/oauth2/authorization"}
+			Host: "http://localhost:9000",
+			Path: "/dialog/authorize"}
 	}
 	if p.RedeemURL.String() == "" {
 		p.RedeemURL = &url.URL{Scheme: "https",
-			Host: "www.linkedin.com",
-			Path: "/uas/oauth2/accessToken"}
+			Host: "http://localhost:9000",
+			Path: "/oauth/token"}
 	}
 	if p.ProfileURL.String() == "" {
 		p.ProfileURL = &url.URL{Scheme: "https",
-			Host: "www.linkedin.com",
-			Path: "/v1/people/~/email-address"}
+			Host: "http://localhost:9000",
+			Path: "/api/users/me"}
 	}
 	if p.ValidateURL.String() == "" {
 		p.ValidateURL = p.ProfileURL
 	}
 	if p.Scope == "" {
-		p.Scope = "r_emailaddress r_basicprofile"
+		p.Scope = "*"
 	}
-	return &LinkedInProvider{ProviderData: p}
+	return &HotelxProvider{ProviderData: p}
 }
 
-func getLinkedInHeader(access_token string) http.Header {
+func getHotelxInHeader(access_token string) http.Header {
 	header := make(http.Header)
 	header.Set("Accept", "application/json")
 	header.Set("x-li-format", "json")
@@ -47,7 +47,7 @@ func getLinkedInHeader(access_token string) http.Header {
 	return header
 }
 
-func (p *LinkedInProvider) GetEmailAddress(s *SessionState) (string, error) {
+func (p *HotelxProvider) GetEmailAddress(s *SessionState) (string, error) {
 	if s.AccessToken == "" {
 		return "", errors.New("missing access token")
 	}
@@ -55,7 +55,7 @@ func (p *LinkedInProvider) GetEmailAddress(s *SessionState) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	req.Header = getLinkedInHeader(s.AccessToken)
+	req.Header = getHotelxInHeader(s.AccessToken)
 
 	json, err := api.Request(req)
 	if err != nil {
@@ -69,6 +69,6 @@ func (p *LinkedInProvider) GetEmailAddress(s *SessionState) (string, error) {
 	return email, nil
 }
 
-func (p *LinkedInProvider) ValidateSessionState(s *SessionState) bool {
-	return validateToken(p, s.AccessToken, getLinkedInHeader(s.AccessToken))
+func (p *HotelxProvider) ValidateSessionState(s *SessionState) bool {
+	return validateToken(p, s.AccessToken, getHotelxInHeader(s.AccessToken))
 }
