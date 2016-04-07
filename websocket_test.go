d67c0f62d9ebc856b198ba86aa02657660606ec6
Oliver Soell <oliver@soell.net>
2016-04-07 11:37:07 -0500

    Handle upgrade headers more fully

--- websocket_test.go
+++ websocket_test.go
@@ -22,5 +22,26 @@ func TestCopyHeader(t *testing.T) {
 	for key, _ := range src {
 		copyHeader(&dst, src, key)
 	}
-	assert.Equal(t, dst, expected)
+	assert.Equal(t, expected, dst)
+}
+
+func TestUpgrade(t *testing.T) {
+	tests := []struct {
+		upgrade         bool
+		connectionValue string
+		upgradeValue    string
+	}{
+		{true, "Upgrade", "Websocket"},
+		{true, "keepalive, Upgrade", "websocket"},
+		{false, "", "websocket"},
+		{false, "keepalive, Upgrade", ""},
+	}
+
+	for _, tt := range tests {
+		req := new(http.Request)
+		req.Header = http.Header{}
+		req.Header.Set(ConnectionHeaderKey, tt.connectionValue)
+		req.Header.Set(UpgradeHeaderKey, tt.upgradeValue)
+		assert.Equal(t, tt.upgrade, isWebsocketRequest(req))
+	}
 }
