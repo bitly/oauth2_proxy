d95b37ef31de83662bf5d0b6d65c2701dd19836b
iyerusad <iyerusad@users.noreply.github.com>
2017-03-30 15:52:42 -0600

    Update options.go
    Bring the parameters expected in the config file to be same as the command line parameters.

--- options.go
+++ options.go
@@ -19,60 +19,60 @@ import (
 // Configuration Options that can be set by Command Line Flag, or Config File
 type Options struct {
 	ProxyPrefix  string `flag:"proxy-prefix" cfg:"proxy-prefix"`
-	HttpAddress  string `flag:"http-address" cfg:"http_address"`
-	HttpsAddress string `flag:"https-address" cfg:"https_address"`
-	RedirectURL  string `flag:"redirect-url" cfg:"redirect_url"`
-	ClientID     string `flag:"client-id" cfg:"client_id" env:"OAUTH2_PROXY_CLIENT_ID"`
-	ClientSecret string `flag:"client-secret" cfg:"client_secret" env:"OAUTH2_PROXY_CLIENT_SECRET"`
-	TLSCertFile  string `flag:"tls-cert" cfg:"tls_cert_file"`
-	TLSKeyFile   string `flag:"tls-key" cfg:"tls_key_file"`
+	HttpAddress  string `flag:"http-address" cfg:"http-address"`
+	HttpsAddress string `flag:"https-address" cfg:"https-address"`
+	RedirectURL  string `flag:"redirect-url" cfg:"redirect-url"`
+	ClientID     string `flag:"client-id" cfg:"client-id" env:"OAUTH2_PROXY_CLIENT_ID"`
+	ClientSecret string `flag:"client-secret" cfg:"client-secret" env:"OAUTH2_PROXY_CLIENT_SECRET"`
+	TLSCertFile  string `flag:"tls-cert" cfg:"tls-cert"`
+	TLSKeyFile   string `flag:"tls-key" cfg:"tls-key"`
 
-	AuthenticatedEmailsFile  string   `flag:"authenticated-emails-file" cfg:"authenticated_emails_file"`
-	AzureTenant              string   `flag:"azure-tenant" cfg:"azure_tenant"`
-	EmailDomains             []string `flag:"email-domain" cfg:"email_domains"`
-	GitHubOrg                string   `flag:"github-org" cfg:"github_org"`
-	GitHubTeam               string   `flag:"github-team" cfg:"github_team"`
-	GoogleGroups             []string `flag:"google-group" cfg:"google_group"`
-	GoogleAdminEmail         string   `flag:"google-admin-email" cfg:"google_admin_email"`
-	GoogleServiceAccountJSON string   `flag:"google-service-account-json" cfg:"google_service_account_json"`
-	HtpasswdFile             string   `flag:"htpasswd-file" cfg:"htpasswd_file"`
-	DisplayHtpasswdForm      bool     `flag:"display-htpasswd-form" cfg:"display_htpasswd_form"`
-	CustomTemplatesDir       string   `flag:"custom-templates-dir" cfg:"custom_templates_dir"`
+	AuthenticatedEmailsFile  string   `flag:"authenticated-emails-file" cfg:"authenticated-emails-file"`
+	AzureTenant              string   `flag:"azure-tenant" cfg:"azure-tenant"`
+	EmailDomains             []string `flag:"email-domain" cfg:"email-domain"`
+	GitHubOrg                string   `flag:"github-org" cfg:"github-org"`
+	GitHubTeam               string   `flag:"github-team" cfg:"github-team"`
+	GoogleGroups             []string `flag:"google-group" cfg:"google-group"`
+	GoogleAdminEmail         string   `flag:"google-admin-email" cfg:"google-admin-email"`
+	GoogleServiceAccountJSON string   `flag:"google-service-account-json" cfg:"google-service-account-json"`
+	HtpasswdFile             string   `flag:"htpasswd-file" cfg:"htpasswd-file"`
+	DisplayHtpasswdForm      bool     `flag:"display-htpasswd-form" cfg:"display-htpasswd-form"`
+	CustomTemplatesDir       string   `flag:"custom-templates-dir" cfg:"custom-templates-dir"`
 	Footer                   string   `flag:"footer" cfg:"footer"`
 
-	CookieName     string        `flag:"cookie-name" cfg:"cookie_name" env:"OAUTH2_PROXY_COOKIE_NAME"`
-	CookieSecret   string        `flag:"cookie-secret" cfg:"cookie_secret" env:"OAUTH2_PROXY_COOKIE_SECRET"`
-	CookieDomain   string        `flag:"cookie-domain" cfg:"cookie_domain" env:"OAUTH2_PROXY_COOKIE_DOMAIN"`
-	CookieExpire   time.Duration `flag:"cookie-expire" cfg:"cookie_expire" env:"OAUTH2_PROXY_COOKIE_EXPIRE"`
-	CookieRefresh  time.Duration `flag:"cookie-refresh" cfg:"cookie_refresh" env:"OAUTH2_PROXY_COOKIE_REFRESH"`
-	CookieSecure   bool          `flag:"cookie-secure" cfg:"cookie_secure"`
-	CookieHttpOnly bool          `flag:"cookie-httponly" cfg:"cookie_httponly"`
+	CookieName     string        `flag:"cookie-name" cfg:"cookie-name" env:"OAUTH2_PROXY_COOKIE_NAME"`
+	CookieSecret   string        `flag:"cookie-secret" cfg:"cookie-secret" env:"OAUTH2_PROXY_COOKIE_SECRET"`
+	CookieDomain   string        `flag:"cookie-domain" cfg:"cookie-domain" env:"OAUTH2_PROXY_COOKIE_DOMAIN"`
+	CookieExpire   time.Duration `flag:"cookie-expire" cfg:"cookie-expire" env:"OAUTH2_PROXY_COOKIE_EXPIRE"`
+	CookieRefresh  time.Duration `flag:"cookie-refresh" cfg:"cookie-refresh" env:"OAUTH2_PROXY_COOKIE_REFRESH"`
+	CookieSecure   bool          `flag:"cookie-secure" cfg:"cookie-secure"`
+	CookieHttpOnly bool          `flag:"cookie-httponly" cfg:"cookie-httponly"`
 
-	Upstreams             []string `flag:"upstream" cfg:"upstreams"`
+	Upstreams             []string `flag:"upstream" cfg:"upstream"`
 	SkipAuthRegex         []string `flag:"skip-auth-regex" cfg:"skip_auth_regex"`
-	PassBasicAuth         bool     `flag:"pass-basic-auth" cfg:"pass_basic_auth"`
-	BasicAuthPassword     string   `flag:"basic-auth-password" cfg:"basic_auth_password"`
-	PassAccessToken       bool     `flag:"pass-access-token" cfg:"pass_access_token"`
-	PassHostHeader        bool     `flag:"pass-host-header" cfg:"pass_host_header"`
-	SkipProviderButton    bool     `flag:"skip-provider-button" cfg:"skip_provider_button"`
-	PassUserHeaders       bool     `flag:"pass-user-headers" cfg:"pass_user_headers"`
-	SSLInsecureSkipVerify bool     `flag:"ssl-insecure-skip-verify" cfg:"ssl_insecure_skip_verify"`
-	SetXAuthRequest       bool     `flag:"set-xauthrequest" cfg:"set_xauthrequest"`
+	PassBasicAuth         bool     `flag:"pass-basic-auth" cfg:"pass-basic-auth"`
+	BasicAuthPassword     string   `flag:"basic-auth-password" cfg:"basic-auth-password"`
+	PassAccessToken       bool     `flag:"pass-access-token" cfg:"pass-access-token"`
+	PassHostHeader        bool     `flag:"pass-host-header" cfg:"pass-host-header"`
+	SkipProviderButton    bool     `flag:"skip-provider-button" cfg:"skip-provider-button"`
+	PassUserHeaders       bool     `flag:"pass-user-headers" cfg:"pass-user-headers"`
+	SSLInsecureSkipVerify bool     `flag:"ssl-insecure-skip-verify" cfg:"ssl-insecure-skip-verify"`
+	SetXAuthRequest       bool     `flag:"set-xauthrequest" cfg:"set-xauthrequest"`
 
 	// These options allow for other providers besides Google, with
 	// potential overrides.
 	Provider          string `flag:"provider" cfg:"provider"`
-	LoginURL          string `flag:"login-url" cfg:"login_url"`
-	RedeemURL         string `flag:"redeem-url" cfg:"redeem_url"`
-	ProfileURL        string `flag:"profile-url" cfg:"profile_url"`
+	LoginURL          string `flag:"login-url" cfg:"login-url"`
+	RedeemURL         string `flag:"redeem-url" cfg:"redeem-url"`
+	ProfileURL        string `flag:"profile-url" cfg:"profile-url"`
 	ProtectedResource string `flag:"resource" cfg:"resource"`
-	ValidateURL       string `flag:"validate-url" cfg:"validate_url"`
+	ValidateURL       string `flag:"validate-url" cfg:"validate-url"`
 	Scope             string `flag:"scope" cfg:"scope"`
-	ApprovalPrompt    string `flag:"approval-prompt" cfg:"approval_prompt"`
+	ApprovalPrompt    string `flag:"approval-prompt" cfg:"approval-prompt"`
 
-	RequestLogging bool `flag:"request-logging" cfg:"request_logging"`
+	RequestLogging bool `flag:"request-logging" cfg:"request-logging"`
 
-	SignatureKey string `flag:"signature-key" cfg:"signature_key" env:"OAUTH2_PROXY_SIGNATURE_KEY"`
+	SignatureKey string `flag:"signature-key" cfg:"signature-key" env:"OAUTH2_PROXY_SIGNATURE_KEY"`
 
 	// internal values that are set after config validation
 	redirectURL   *url.URL
@@ -119,7 +119,7 @@ func parseURL(to_parse string, urltype string, msgs []string) (*url.URL, []strin
 
 func (o *Options) Validate() error {
 	msgs := make([]string, 0)
-	if len(o.Upstreams) < 1 {
+	if len(o.Upstream) < 1 {
 		msgs = append(msgs, "missing setting: upstream")
 	}
 	if o.CookieSecret == "" {
@@ -131,13 +131,13 @@ func (o *Options) Validate() error {
 	if o.ClientSecret == "" {
 		msgs = append(msgs, "missing setting: client-secret")
 	}
-	if o.AuthenticatedEmailsFile == "" && len(o.EmailDomains) == 0 && o.HtpasswdFile == "" {
+	if o.AuthenticatedEmailsFile == "" && len(o.EmailDomain) == 0 && o.HtpasswdFile == "" {
 		msgs = append(msgs, "missing setting for email validation: email-domain or authenticated-emails-file required.\n      use email-domain=* to authorize all email addresses")
 	}
 
 	o.redirectURL, msgs = parseURL(o.RedirectURL, "redirect", msgs)
 
-	for _, u := range o.Upstreams {
+	for _, u := range o.Upstream {
 		upstreamURL, err := url.Parse(u)
 		if err != nil {
 			msgs = append(msgs, fmt.Sprintf(
