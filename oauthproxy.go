d35b98911d7abed6f838df4083b3f1e83cbf17c9
Bastian Eicher <bastian@eicher.net>
2017-05-22 16:53:28 +0200

    Fix handling of redirect URI with --skip-provider-button
    Fixes #327

--- oauthproxy.go
+++ oauthproxy.go
@@ -410,6 +410,11 @@ func (p *OAuthProxy) ManualSignIn(rw http.ResponseWriter, req *http.Request) (st
 }
 
 func (p *OAuthProxy) GetRedirect(req *http.Request) (redirect string, err error) {
+	if p.SkipProviderButton {
+		redirect = req.RequestURI
+		return
+	}
+
 	err = req.ParseForm()
 	if err != nil {
 		return
