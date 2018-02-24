f762a4d2966d0f2d6d57698a0afe68aa7a6ab6e9
Christopher Snell <chris.snell@gmail.com>
2018-02-24 16:13:44 -0600

    Rework entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,101 +1,79 @@
 #!/bin/bash
 set -e
 
-# first arg is `-f` or `--some-option`
-if [ "${1:0:1}" = '-' ]; then
-    set -- oauth2_proxy "$@"
+export PATH=/bin:/usr/bin:/usr/local/bin:/usr/sbin:/
+
+if [ ! -z ${OAUTH2_PROXY_CLIENT_ID+x} ]; then
+   PROXY_ARGS="${PROXY_ARGS} -client-id ${OAUTH2_PROXY_CLIENT_ID}"
 fi
 
-if [ "$1" = 'oauth2_proxy' ]; then
-    # if no configfile is provided, generate one based on the environment variables
-    if [ ! -f /conf/oauth2_proxy.cfg ]; then
-        # if undefined, populate selected environment variables with sane defaults
-        : ${OAUTH2_PROXY_EMAIL_DOMAIN="*"}
-        : ${OAUTH2_PROXY_HTTP_ADDRESS="0.0.0.0:4180"}
-        : ${OAUTH2_PROXY_COOKIE_SECRET="$(date | md5sum | awk '{ print $1 }')"}
-
-        # available config variables '
-        VARS="
-            approval-prompt
-            authenticated-emails-file
-            azure-tenant
-            basic-auth-password
-            client-id
-            client-secret
-            config
-            cookie-domain
-            cookie-expire
-            cookie-httponly
-            cookie-name
-            cookie-refresh
-            cookie-secret
-            cookie-secure
-            custom-templates-dir
-            display-htpasswd-form
-            email-domain
-            github-org
-            github-team
-            google-admin-email
-            google-group
-            google-service-account-json
-            htpasswd-file
-            http-address
-            https-address
-            login-url
-            pass-access-token
-            pass-basic-auth
-            pass-host-header
-            pass-user-headers
-            profile-url
-            provider
-            proxy-prefix
-            redeem-url
-            redirect-url
-            resource
-            request-logging
-            scope
-            sign-aws-request-region
-            sign-aws-request-service
-            signature-key
-            skip-auth-regex
-            skip-auth-preflight
-            skip-provider-button
-            ssl-insecure-skip-verify
-            tls-cert
-            tls-key
-            upstream
-            validate-url"
-        for var in ${VARS}; do
-            # config file variable names are with _ instead of -
-            var="$(echo ${var} | sed -e 's/-/_/g')"
-            # convert config variable name to bash compatible variable name
-            # e.g. "approval-prompt" becomes "OAUTH2_PROXY_APPROVAL_PROMPT"
-            env_var="OAUTH2_PROXY_$(echo ${var} | tr '[:lower:]' '[:upper:]')"
-            # test for environment variable existence
-            if [ ! -z ${!env_var+x} ]; then
-                # list values need to be treated especially
-                if [ "${var}" == "upstream" ] \
-                || [ "${var}" == "email_domain" ]; then
-                    # plural and list
-                    echo "${var}s = [ " >> /conf/oauth2_proxy.cfg
-                    # disable * expansion
-                    set -f
-                    for v in ${!env_var}; do
-                        echo "  \"${v}\"," >> /conf/oauth2_proxy.cfg
-                    done
-                    set +f
-                    echo "]" >> /conf/oauth2_proxy.cfg
-                else
-                    # unfortunately ${!var} is only available in bash and not sh
-                    # that's why the alpine container installs bash as runtime dependency
-                    echo "${var} = \"${!env_var}\"" >> /conf/oauth2_proxy.cfg
-                fi
-            fi
-        done
-        cat /conf/oauth2_proxy.cfg
-    fi
+if [ ! -z ${OAUTH2_PROXY_CLIENT_SECRET+x} ]; then
+   PROXY_ARGS="${PROXY_ARGS} -client-secret ${OAUTH2_PROXY_CLIENT_SECRET}"
 fi
+
+if [ ! -z ${OAUTH2_PROXY_COOKIE_EXPIRE+x} ]; then
+   PROXY_ARGS="${PROXY_ARGS} -cookie-expire ${OAUTH2_PROXY_COOKIE_EXPIRE}"
+fi
+
+if [ ! -z ${OAUTH2_PROXY_COOKIE_SECRET+x} ]; then
+   PROXY_ARGS="${PROXY_ARGS} -cookie-secret ${OAUTH2_PROXY_COOKIE_SECRET}"
+fi
+
+if [ ! -z ${OAUTH2_PROXY_EMAIL_DOMAIN+x} ]; then
+   PROXY_ARGS="${PROXY_ARGS} -email-domain ${OAUTH2_PROXY_EMAIL_DOMAIN}"
+fi
+
+if [ ! -z ${OAUTH2_PROXY_GITHUB_TEAM+x} ]; then
+   PROXY_ARGS="${PROXY_ARGS} -github-team ${OAUTH2_PROXY_GITHUB_TEAM}"
+fi
+
+if [ ! -z ${OAUTH2_PROXY_GITHUB_ORG+x} ]; then
+   PROXY_ARGS="${PROXY_ARGS} -github-org ${OAUTH2_PROXY_GITHUB_ORG}"
+fi
+
+if [ ! -z ${OAUTH2_PROXY_HTTP_ADDRESS+x} ]; then
+   PROXY_ARGS="${PROXY_ARGS} -http-address ${OAUTH2_PROXY_HTTP_ADDRESS}"
+fi
+
+if [ ! -z ${OAUTH2_PROXY_HTTPS_ADDRESS+x} ]; then
+   PROXY_ARGS="${PROXY_ARGS} -https-address ${OAUTH2_PROXY_HTTPS_ADDRESS}"
+fi
+
+if [ ! -z ${OAUTH2_PROXY_REDIRECT_URL+x} ]; then
+   PROXY_ARGS="${PROXY_ARGS} -redirect-url ${OAUTH2_PROXY_REDIRECT_URL}"
+fi
+
+if [ ! -z ${OAUTH2_PROXY_TLS_CERT+x} ]; then
+   PROXY_ARGS="${PROXY_ARGS} -tls-cert ${OAUTH2_PROXY_TLS_CERT}"
+fi
+
+if [ ! -z ${OAUTH2_PROXY_TLS_KEY+x} ]; then
+   PROXY_ARGS="${PROXY_ARGS} -tls-key ${OAUTH2_PROXY_TLS_KEY}"
+fi
+
+if [ ! -z ${OAUTH2_PROXY_PROVIDER+x} ]; then
+   PROXY_ARGS="${PROXY_ARGS} -provider ${OAUTH2_PROXY_PROVIDER}"
+fi
+
+if [ ! -z ${OAUTH2_PROXY_UPSTREAM+x} ]; then
+   PROXY_ARGS="${PROXY_ARGS} -upstream ${OAUTH2_PROXY_UPSTREAM}"
+fi
+
+if [ ! -z ${OAUTH2_PROXY_SIGN_AWS_REQUEST_REGION+x} ]; then
+   PROXY_ARGS="${PROXY_ARGS} -sign-aws-request-region ${OAUTH2_PROXY_SIGN_AWS_REQUEST_REGION}"
+fi
+
+if [ ! -z ${OAUTH2_PROXY_SIGN_AWS_REQUEST_SERVICE+x} ]; then
+   PROXY_ARGS="${PROXY_ARGS} -sign-aws-request-service ${OAUTH2_PROXY_SIGN_AWS_REQUEST_SERVICE}"
+fi
+
+if [ ! -z ${OAUTH2_PROXY_AWS_ACCESS_KEY+x} ]; then
+   AWS_ACCESS_KEY=$OAUTH2_PROXY_AWS_ACCESS_KEY
+fi
+
+if [ ! -z ${OAUTH2_PROXY_AWS_SECRET_ACCESS_KEY+x} ]; then
+   AWS_SECRET_ACCESS_KEY=$OAUTH2_PROXY_AWS_SECRET_ACCESS_KEY
+fi 
 
 echo "Launching oauth2_proxy..."
-ls -l /conf
-exec /gosu nobody "$@"
+exec /gosu nobody /oauth2_proxy ${PROXY_ARGS}
