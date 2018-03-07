7ed0efc992a11cb9de83e847535d8ba894db57d4
Christopher Snell <chris.snell@gmail.com>
2018-03-07 10:17:24 -0600

    Only quote args for email domain

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,67 +4,67 @@ set -e
 export PATH=/bin:/usr/bin:/usr/local/bin:/usr/sbin:/
 
 if [ ! -z ${OAUTH2_PROXY_CLIENT_ID+x} ] && [ ! -z $OAUTH2_PROXY_CLIENT_ID ]; then
-   PROXY_ARGS="${PROXY_ARGS} --client-id=\"${OAUTH2_PROXY_CLIENT_ID}\""
+   PROXY_ARGS="${PROXY_ARGS} --client-id=${OAUTH2_PROXY_CLIENT_ID}"
 fi
 
 if [ ! -z ${OAUTH2_PROXY_CLIENT_SECRET+x} ] && [ ! -z $OAUTH2_PROXY_CLIENT_SECRET ]; then
-   PROXY_ARGS="${PROXY_ARGS} --client-secret=\"${OAUTH2_PROXY_CLIENT_SECRET}\""
+   PROXY_ARGS="${PROXY_ARGS} --client-secret=${OAUTH2_PROXY_CLIENT_SECRET}"
 fi
 
 if [ ! -z ${OAUTH2_PROXY_COOKIE_EXPIRE+x} ] && [ ! -z $OAUTH2_PROXY_COOKIE_EXPIRE ]; then
-   PROXY_ARGS="${PROXY_ARGS} --cookie-expire=\"${OAUTH2_PROXY_COOKIE_EXPIRE}\""
+   PROXY_ARGS="${PROXY_ARGS} --cookie-expire=${OAUTH2_PROXY_COOKIE_EXPIRE}"
 fi
 
 if [ ! -z ${OAUTH2_PROXY_COOKIE_SECRET+x} ] && [ ! -z $OAUTH2_PROXY_COOKIE_SECRET ]; then
-   PROXY_ARGS="${PROXY_ARGS} --cookie-secret=\"${OAUTH2_PROXY_COOKIE_SECRET}\""
+   PROXY_ARGS="${PROXY_ARGS} --cookie-secret=${OAUTH2_PROXY_COOKIE_SECRET}"
 fi
 
 if [ ! -z ${OAUTH2_PROXY_EMAIL_DOMAIN+x} ] && [ ! -z $OAUTH2_PROXY_EMAIL_DOMAIN ]; then
-   PROXY_ARGS="${PROXY_ARGS} --email-domain=\"${OAUTH2_PROXY_EMAIL_DOMAIN}\""
+   PROXY_ARGS="${PROXY_ARGS} --email-domain='${OAUTH2_PROXY_EMAIL_DOMAIN}'"
 fi
 
 if [ ! -z ${OAUTH2_PROXY_GITHUB_TEAM+x} ] && [ ! -z $OAUTH2_PROXY_GITHUB_TEAM ]; then
-   PROXY_ARGS="${PROXY_ARGS} --github-team=\"${OAUTH2_PROXY_GITHUB_TEAM}\""
+   PROXY_ARGS="${PROXY_ARGS} --github-team=${OAUTH2_PROXY_GITHUB_TEAM}"
 fi
 
 if [ ! -z ${OAUTH2_PROXY_GITHUB_ORG+x} ] && [ ! -z $OAUTH2_PROXY_GITHUB_ORG ]; then
-   PROXY_ARGS="${PROXY_ARGS} --github-org=\"${OAUTH2_PROXY_GITHUB_ORG}\""
+   PROXY_ARGS="${PROXY_ARGS} --github-org=${OAUTH2_PROXY_GITHUB_ORG}"
 fi
 
 if [ ! -z ${OAUTH2_PROXY_HTTP_ADDRESS+x} ] && [ ! -z $OAUTH2_PROXY_HTTP_ADDRESS ]; then
-   PROXY_ARGS="${PROXY_ARGS} --http-address=\"${OAUTH2_PROXY_HTTP_ADDRESS}\""
+   PROXY_ARGS="${PROXY_ARGS} --http-address=${OAUTH2_PROXY_HTTP_ADDRESS}"
 fi
 
 if [ ! -z ${OAUTH2_PROXY_HTTPS_ADDRESS+x} ] && [ ! -z $OAUTH2_PROXY_HTTPS_ADDRESS ]; then
-   PROXY_ARGS="${PROXY_ARGS} --https-address=\"${OAUTH2_PROXY_HTTPS_ADDRESS}\""
+   PROXY_ARGS="${PROXY_ARGS} --https-address=${OAUTH2_PROXY_HTTPS_ADDRESS}"
 fi
 
 if [ ! -z ${OAUTH2_PROXY_REDIRECT_URL+x} ] && [ ! -z $OAUTH2_PROXY_REDIRECT_URL ]; then
-   PROXY_ARGS="${PROXY_ARGS} --redirect-url=\"${OAUTH2_PROXY_REDIRECT_URL}\""
+   PROXY_ARGS="${PROXY_ARGS} --redirect-url=${OAUTH2_PROXY_REDIRECT_URL}"
 fi
 
 if [ ! -z ${OAUTH2_PROXY_TLS_CERT+x} ] && [ ! -z $OAUTH2_PROXY_TLS_CERT ]; then
-   PROXY_ARGS="${PROXY_ARGS} --tls-cert=\"${OAUTH2_PROXY_TLS_CERT}\""
+   PROXY_ARGS="${PROXY_ARGS} --tls-cert=${OAUTH2_PROXY_TLS_CERT}"
 fi
 
 if [ ! -z ${OAUTH2_PROXY_TLS_KEY+x} ] && [ ! -z $OAUTH2_PROXY_TLS_KEY ]; then
-   PROXY_ARGS="${PROXY_ARGS} --tls-key=\"${OAUTH2_PROXY_TLS_KEY}\""
+   PROXY_ARGS="${PROXY_ARGS} --tls-key=${OAUTH2_PROXY_TLS_KEY}"
 fi
 
 if [ ! -z ${OAUTH2_PROXY_PROVIDER+x} ] && [ ! -z $OAUTH2_PROXY_PROVIDER ]; then
-   PROXY_ARGS="${PROXY_ARGS} --provider=\"${OAUTH2_PROXY_PROVIDER}\""
+   PROXY_ARGS="${PROXY_ARGS} --provider=${OAUTH2_PROXY_PROVIDER}"
 fi
 
 if [ ! -z ${OAUTH2_PROXY_UPSTREAM+x} ] && [ ! -z $OAUTH2_PROXY_UPSTREAM ]; then
-   PROXY_ARGS="${PROXY_ARGS} --upstream=\"${OAUTH2_PROXY_UPSTREAM}\""
+   PROXY_ARGS="${PROXY_ARGS} --upstream=${OAUTH2_PROXY_UPSTREAM}"
 fi
 
 if [ ! -z ${OAUTH2_PROXY_SIGN_AWS_REQUEST_REGION+x} ] && [ ! -z $OAUTH2_PROXY_SIGN_AWS_REQUEST_REGION ]; then
-   PROXY_ARGS="${PROXY_ARGS} --sign-aws-request-region=\"${OAUTH2_PROXY_SIGN_AWS_REQUEST_REGION}\""
+   PROXY_ARGS="${PROXY_ARGS} --sign-aws-request-region=${OAUTH2_PROXY_SIGN_AWS_REQUEST_REGION}"
 fi
 
 if [ ! -z ${OAUTH2_PROXY_SIGN_AWS_REQUEST_SERVICE+x} ] && [ ! -z $OAUTH2_PROXY_SIGN_AWS_REQUEST_SERVICE ]; then
-   PROXY_ARGS="${PROXY_ARGS} --sign-aws-request-service=\"${OAUTH2_PROXY_SIGN_AWS_REQUEST_SERVICE}\""
+   PROXY_ARGS="${PROXY_ARGS} --sign-aws-request-service=${OAUTH2_PROXY_SIGN_AWS_REQUEST_SERVICE}"
 fi
 
 if [ ! -z ${OAUTH2_PROXY_AWS_ACCESS_KEY+x} ] && [ ! -z $OAUTH2_PROXY_AWS_ACCESS_KEY ]; then
